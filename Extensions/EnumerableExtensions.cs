ca5b39ccd5229ca89c2b85ee1244a50ea9afd344
Florian Fida <f+git@fida.biz>
2019-09-20 21:15:24 +0200

    Add project files.

---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Extensions {
+    public static class EnumerableExtensions {
+
+        internal static Func<TClass, object> CompileGetter<TClass>(string propertyName) {
+            var param = Expression.Parameter(typeof(TClass));
+            var body = Expression.Convert(Expression.Property(param, propertyName), typeof(object));
+            return Expression.Lambda<Func<TClass, object>>(body,param).Compile();
+        }
+
+        internal static Action<TClass, object> CompileSetter<TClass>(string propertyName) {
+            var propertyInfo = typeof(TClass).GetProperty(propertyName);
+            if (propertyInfo == null) return null;
+            var targetType = propertyInfo.DeclaringType;
+            if (targetType == null) return null;
+            var param = Expression.Parameter(targetType);
+            var value = Expression.Parameter(typeof(object));
+            var body = Expression.Call(param, propertyInfo.GetSetMethod(), Expression.Convert(value, propertyInfo.PropertyType));
+            return Expression.Lambda<Action<TClass, object>>(body, param, value).Compile();
+        }
+
+        public static ICollection<IDictionary<string, object>> ToDictionaryCollection<T>(this IEnumerable<T> enumerable) {
+            if (enumerable is null) return null;
+            var type = typeof(T);
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            if (!properties.Any()) return null;
+            var getters = properties.Where(p => p.CanRead).Select(p => new {
+                p.Name, GetValue = CompileGetter<T>(p.Name),
+            }).ToList();
+            var resp = new Collection<IDictionary<string, object>>();
+
+            foreach (var row in enumerable) {
+                var rowDict = new Dictionary<string, object>();
+                foreach (var getter in getters) {
+                    rowDict[getter.Name] = getter.GetValue(row);
+                }
+                resp.Add(rowDict);
+            }
+
+            return resp;
+        }
+
+        public static DataTable ToDataTable<T>(this IEnumerable<T> collection) {
+            var dataTable = new DataTable();
+
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                      .Where(p => p.CanRead)
+                                      .Select(p => new {
+                                           p.Name,
+                                           p.PropertyType,
+                                           GetValue = CompileGetter<T>(p.Name),
+                                       }).ToList();
+
+            foreach (var property in properties) {
+                dataTable.Columns.Add(property.Name, property.PropertyType);
+            }
+            
+            foreach (var row in collection) {
+                var dtRow = new object[properties.Count];
+                for (var i = 0; i < properties.Count; i++) {
+                    dtRow[i] = properties[i].GetValue(row);
+                }
+                dataTable.Rows.Add(dtRow);
+            }
+
+            return dataTable;
+        }
+    }
+}
---
+++ ReflectionSpeed/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Extensions;
+
+namespace ReflectionSpeed
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            for (var i = 0; i < 10; i++) {
+                DirectCall();
+                WithLambda();
+                WithReflectionDelegate();
+                WithReflection();
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(100);
+            }
+            
+        }
+
+        static ICollection<MyClass> GetCollection() => Enumerable.Repeat(new MyClass(), 10000000).ToList();
+
+        static void DirectCall() {
+
+            var myClassList = GetCollection();
+
+            var sw = new Stopwatch();
+            
+            object aux = 0;    
+            sw.Start();
+            foreach (var obj in myClassList)    
+            {    
+                aux = obj.Number;    
+                obj.Number = 3;    
+            }    
+            sw.Stop();
+            Console.WriteLine($"DirectCall: {sw.Elapsed.TotalMilliseconds} msec");
+        }
+
+        static void WithReflection() {
+            var list = GetCollection();
+            var sw = new Stopwatch();
+            
+            object aux = 0;
+            var pi = list.First().GetType().GetProperty("Number");
+            sw.Start();
+            foreach (var obj in list) {
+                aux = pi.GetValue(obj);
+                pi.SetValue(obj, 3);
+            }
+            sw.Stop();
+            Console.WriteLine($"WithReflection: {sw.Elapsed.TotalMilliseconds} msec");
+        }
+
+
+        static void WithReflectionDelegate() {
+            var myClassList = GetCollection();
+            var sw = new Stopwatch();
+            
+            object aux = 0;
+            var propertyInfo = myClassList.First().GetType().GetProperty("Number");
+            var setter = (Action<MyClass, int>)Delegate.CreateDelegate(typeof(Action<MyClass, int>), null, propertyInfo.GetSetMethod());
+            var getter = (Func<MyClass, int>)Delegate.CreateDelegate(typeof(Func<MyClass, int>), null, propertyInfo.GetGetMethod());
+           
+            sw.Start();
+            foreach (var obj in myClassList)  
+            {  
+                aux = getter(obj);  
+                setter(obj, 3);  
+            }  
+            sw.Stop();
+            Console.WriteLine($"WithReflectionDelegate: {sw.Elapsed.TotalMilliseconds} msec");
+        }
+
+        //static Func<TClass, object> CompileGetter<TClass>(string propertyName) {
+        //    var param = Expression.Parameter(typeof(TClass));
+        //    var body = Expression.Convert(Expression.Property(param, propertyName), typeof(object));
+        //    return Expression.Lambda<Func<TClass, object>>(body,param).Compile();
+        //}
+
+        //static Action<TClass, object> CompileSetter<TClass>(string propertyName) {
+        //    var propertyInfo = typeof(TClass).GetProperty(propertyName);
+        //    if (propertyInfo == null) return null;
+        //    var targetType = propertyInfo.DeclaringType;
+        //    if (targetType == null) return null;
+        //    var param = Expression.Parameter(targetType);
+        //    var value = Expression.Parameter(typeof(object));
+        //    var body = Expression.Call(param, propertyInfo.GetSetMethod(), Expression.Convert(value, propertyInfo.PropertyType));
+        //    return Expression.Lambda<Action<TClass, object>>(body, param, value).Compile();
+        //}
+
+
+        static void WithLambda() {
+            var myClassList = GetCollection();
+            var sw = new Stopwatch();
+            
+            object aux = 0;
+            
+            var setter = EnumerableExtensions.CompileSetter<MyClass>("Number");
+            var getter = EnumerableExtensions.CompileGetter<MyClass>("Number");
+
+            sw.Start();
+            foreach (var obj in myClassList) {
+                
+                aux = getter(obj);
+                setter(obj, 3);
+            }  
+            sw.Stop();
+            Console.WriteLine($"WithLambda: {sw.Elapsed.TotalMilliseconds} msec");
+        }
+    }
+
+    public class MyClass    
+    {    
+        public int Number { get; set; }    
+    }
+}
